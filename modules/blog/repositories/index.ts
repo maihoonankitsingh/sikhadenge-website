78b64fc3eac28b8b9f36de8fd0c38ba973b07e4c
ANKIT SINGH <251703707+maihoonankitsingh@users.noreply.github.com>
2026-07-29 21:34:18 +0530

    Export imported Blog review repository

--- modules/blog/repositories/index.ts
+++ modules/blog/repositories/index.ts
@@ -1,4 +1,12 @@
 export {
+  getImportedBlogRecordBySlugForReview,
+} from "@/modules/blog/repositories/imported-page-repository";
+
+export type {
+  ImportedBlogReviewRecord,
+} from "@/modules/blog/repositories/imported-page-repository";
+
+export {
   BlogRepositoryInvariantError,
   CANONICAL_BLOG_WORKSPACE_ID,
   CANONICAL_BLOG_WORKSPACE_KEY,
