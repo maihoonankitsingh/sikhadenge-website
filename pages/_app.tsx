c1c4889870624170a2530aab511a970d6dbbf778
ANKIT SINGH <251703707+maihoonankitsingh@users.noreply.github.com>
2026-08-21 09:10:05 +0530

    Phase 5: load separate workshop funnel styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import "../styles/funnel.css";
 import "../styles/funnel-confirmation.css";
 import "../styles/funnel-checkout.css";
+import "../styles/workshop-funnel.css";
 import Header from "../components/Header";
 
 type FunnelAwarePage = AppProps["Component"] & {
